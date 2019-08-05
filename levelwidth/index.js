c8556960c6f186feff68cd878744b2915700565a
Heunsig <heun3344@gmail.com>
2019-08-05 15:57:20 +0900

    Solved the levelwith problem

---
+++ levelwidth/index.js
@@ -0,0 +1,57 @@
+// --- Directions
+// Given the root node of a tree, return
+// an array where each element is the width
+// of the tree at each level.
+// --- Example
+// Given:
+//     0
+//   / |  \
+// 1   2   3
+// |       |
+// 4       5
+// Answer: [1, 3, 2]
+// 
+const Node = require('./node')
+
+function levelWidth(root) {
+  let result = []
+  let arr = [root]
+
+  result.push(arr.length)
+
+  while (arr.length) {
+    let counter = 0
+    let counterOfPreviousLevel = result[result.length - 1]
+
+    while (counterOfPreviousLevel) {
+      let node = arr.shift()
+      
+      counter = counter + node.children.length
+      arr.push(...node.children)
+
+      counterOfPreviousLevel--
+
+    }
+
+    if (counter) {
+      result.push(counter)
+    }
+    
+  }
+
+  return result
+}
+
+// const root = new Node(0);
+// root.add(1);
+// root.add(2);
+// root.add(3);
+// root.children[0].add(4);
+// root.children[2].add(5);
+// root.children[1].add(6);
+// root.children[2].children[0].add(7);
+// // root.children[2].children[0].add(8);
+// root.children[2].children[0].add(9);
+// levelWidth(root)
+
+module.exports = levelWidth;

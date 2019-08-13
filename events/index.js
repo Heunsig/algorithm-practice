7cc3488f4629d4cc80acbd291d25aa737086c676
Heunsig <heun3344@gmail.com>
2019-08-13 21:39:45 +0900

    Solved the events problem

---
+++ events/index.js
@@ -0,0 +1,43 @@
+// --- Directions
+// Create an 'eventing' library out of the
+// Events class.  The Events class should
+// have methods 'on', 'trigger', and 'off'.
+
+class Events {
+  
+  constructor () {
+    this.events = {}
+  }
+
+  // Register an event handler
+  on(eventName, callback) {
+    if (this.events[eventName]) {
+      this.events[eventName] = [...this.events[eventName], callback] 
+    } else {
+      this.events = {
+        ...this.events,
+        ...{ [eventName]: [ callback ] }
+      }
+    }
+  }
+
+  // Trigger all callbacks associated
+  // with a given eventName
+  trigger(eventName) {
+    if (this.events[eventName]) {
+      for(let event of this.events[eventName]) {
+        event()
+      }
+      // this.events[eventName]()
+    }
+  }
+
+  // Remove all event handlers associated
+  // with the given eventName
+  off(eventName) {
+   delete this.events[eventName]
+  }
+}
+
+
+module.exports = Events;

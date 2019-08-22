53ceaf1fa3759c8911cae863d044d621541d4cb6
Heunsig <heun3344@gmail.com>
2019-08-22 14:31:05 +0900

    Removed the solution file

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,13 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-
-const MySolution = require('./MySolution')
-// const Solution = require('./Solution')
-
-
 function maxChar(str) {
-  return MySolution(str)
+  let separated_str = str.split('')
+  let str_arr = {}
+  let biggest_char = ''
+
+  for (let c of separated_str) {
+    str_arr[c] = str_arr[c] + 1 || 1
+  }
+
+  for (let c in str_arr) {
+    if (!biggest_char) {
+      biggest_char = c
+    }
+
+    if (str_arr[c] > str_arr[biggest_char]) {
+      biggest_char = c
+    }
+  }
+
+  return biggest_char
 }
 
 module.exports = maxChar;

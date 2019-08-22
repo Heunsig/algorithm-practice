3d4e7d1e161a77d9cb997b3c4f8d9b9258f85a45
Heunsig <heun3344@gmail.com>
2019-08-22 14:12:36 +0900

    remove solutions

--- capitalize/index.js
+++ capitalize/index.js
@@ -8,12 +8,16 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 
-const MySolution = require('./MySolution')
-// const Solution1 = require('./Solution1')
-// const Solution2 = require('./Solution2')
-
 function capitalize(str) {
-  return MySolution(str)
+  let result = ''
+  let capitalized_words = []
+  const words = str.split(' ')
+  for (let word of words) {
+    let chars = word.split('')
+    capitalized_words.push(chars[0].toUpperCase() + chars.splice(1, chars.length).join(''))
+  }
+
+  return capitalized_words.join(' ')
 }
 
 module.exports = capitalize;

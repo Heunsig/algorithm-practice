88d55c6c74c31a41ecfa5ee74c46b6d81ff7e281
Heunsig <heun3344@gmail.com>
2019-08-22 14:37:21 +0900

    Merged

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,15 +14,67 @@
 //       ' ### '
 //       '#####'
 
+/////////////////////
+// Normal iltering //
+/////////////////////
+function my_solution (n) {
+  const total_columns = (2 * (n - 1)) + 1
+  const center_index = Math.floor(total_columns / 2)
 
-const MySolution1 = require('./MySolution1')
-// const MySolution2 = require('./MySolution2')
-// const Solution1 = require('./Solution1')
-// const Solution2 = require('./Solution2')
+  for (let row = 0 ; row < n ; row++) {
+    const total_hash_in_row = (2 * (row)) + 1
+    const moving_index = (total_hash_in_row - 1) / 2
+    const index_range = [center_index - moving_index, center_index + moving_index]
+
+    let stair = ''
+
+    for (let column = 0 ; column < total_columns ; column++) {
+      if (column >= index_range[0] && column <= index_range[1]) {
+        stair += '#' 
+      } else {
+        stair += ' '
+      }
+    }
+
+    console.log(stair)
+  }
+}
+
+///////////////
+// Recursive //
+///////////////
+function my_solution_2 (n, total_columns=0, center_index=0, row=0, column=0, stair='') {
+  if (row === 0) {
+    total_columns = (2 * n) - 1
+    center_index = Math.floor(total_columns / 2)
+  }
+
+  if (n === row) {
+    return
+  }
+
+  if (column === total_columns) {
+    console.log(stair)
+    return my_solution_2(n, total_columns, center_index, row + 1)
+  }
+
+  const total_hash_in_row = (2 * (row)) + 1
+  const moving_index = (total_hash_in_row - 1) / 2
+  const index_range = [center_index - moving_index, center_index + moving_index]
+
+  if (column >= index_range[0] && column <= index_range[1]) {
+    stair += '#'
+  } else {
+    stair += ' '
+  }
+
+  my_solution_2(n, total_columns, center_index, row, column + 1, stair)
+}
 
 
 function pyramid(n) {
-  MySolution1(n)
+  my_solution(n)
+  // my_solution_2(n)
 }
 
 module.exports = pyramid;

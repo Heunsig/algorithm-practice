d69e32699c71f669b97a1760c857e2dcc8bb5863
Heunsig <heun3344@gmail.com>
2019-08-22 14:11:17 +0900

    delete solutions

--- anagrams/index.js
+++ anagrams/index.js
@@ -9,9 +9,62 @@
 //   anagrams('Hi there', 'Bye there') --> False
 //   
 
-const MySolution = require('./my_solution')
-// const Solution1 = require('./Solution1')
-// const Solution2 = require('./Solution2')
+function MySolution (stringA, stringB) {
+
+  __is_special_symbol = (char) => {
+    const specialChars = "~`!#$%^&*+=-[]\\\';,/{}|\":<>?"
+
+    for (let c of specialChars) {
+      if (c === char) {
+        return true
+      }
+    }
+
+    return false
+  }
+
+  __generate_charmap = (str) => {
+    const charMap = {}
+
+    for (let char of str) {
+      char = char.toLowerCase()
+      if (!__is_special_symbol(char)) {
+        if (charMap[char]) {
+          charMap[char]++
+        } else {
+          charMap[char] = 1
+        }  
+      }
+    }
+
+    return charMap
+  }
+
+  __is_matched = (mapA, mapB) => {
+    if (Object.keys(mapA).length !== Object.keys(mapB).length) {
+      return false
+    }
+
+    for (let o in mapA) {
+      if (mapA[o] !== mapB[o]) {
+        return false
+      }
+    }
+
+    return true
+  } 
+
+  
+  this.execute = () => {
+    const stringA_map = __generate_charmap(stringA)
+    const stringB_map = __generate_charmap(stringB)
+
+    return __is_matched(stringA_map, stringB_map)
+  }
+
+}
+
+module.exports = MySolution
 
 
 function anagrams(stringA, stringB) {

131882bba24edcafcf6b57c4a05109c83e984e82
Heunsig <heun3344@gmail.com>
2019-08-22 14:47:28 +0900

    Deleted useless files

--- weave/index.js
+++ weave/index.js
@@ -24,11 +24,14 @@
 
 const Queue = require('./queue');
 
-const mySolution = require('./mySolution')
-const solution = require('./solution')
-
 function weave(sourceOne, sourceTwo) {
-  return mySolution(sourceOne, sourceTwo, Queue)
+  const q = new Queue()
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    q.add(sourceOne.remove())
+    q.add(sourceTwo.remove())
+  }
+
+  return q
 }
 
 module.exports = weave;

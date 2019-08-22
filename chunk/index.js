516d178d3c3ecd0ae40611fbf653213cb42667d6
Heunsig <heun3344@gmail.com>
2019-08-22 14:13:47 +0900

    removed solutions

--- chunk/index.js
+++ chunk/index.js
@@ -9,12 +9,25 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 
-const MySolution = require('./MySolution')
-// const Solution1 = require('./Solution1')
-// const Solution2 = require('./Solution2')
-
 function chunk(array, size) {
-  return MySolution(array, size)
+  let result_arr = []
+  let chunk_arr = []
+
+  for (let value of array) {
+    if (chunk_arr.length < size) {
+      chunk_arr.push(value)
+    } else {
+      result_arr.push(chunk_arr)
+      chunk_arr = []
+      chunk_arr.push(value)
+    }
+  }
+
+  if (chunk_arr.length !== 0) {
+    result_arr.push(chunk_arr)
+  }
+
+  return result_arr
 }
 
 module.exports = chunk;

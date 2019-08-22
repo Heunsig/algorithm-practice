3e882bcd8d7414bc95d0c29b8e1114ad3cb0b9d2
Heunsig <heun3344@gmail.com>
2019-08-22 14:18:14 +0900

    Removed other solutions

--- fib/Solution2.js
+++
@@ -1,9 +0,0 @@
-function Solution (n) {
-  if (n < 2) {
-    return n
-  }
-
-  return solution_2(n - 1) + solution_2(n - 2)
-}
-
-module.exports = Solution
--- fib/index.js
+++ fib/index.js
@@ -8,13 +8,66 @@
 // Example:
 //   fib(4) === 3
 
-// const MySolution1 = require('./MySolution1')
-// const MySolution2 = require('./MySolution2')
-const MySolution3 = require('./MySolution3')
-// const Solution1 = require('./Solution1')
-// const Solution2 = require('./Solution2')
-// const Solution3 = require('./Solution3')
 
+/////////////////////
+// Normal iltering //
+/////////////////////
+function MySolution (n) {
+  let previous_number = 0
+  let sum = 1
+  let temp = 0
+
+  if (n < 1) {
+    return 0
+  }
+
+  for (let i = 1 ; i < n ; i++) {
+    temp = sum
+    sum = sum + previous_number
+    previous_number = temp
+  }
+
+  return sum
+}
+
+///////////////
+// Recursive //
+///////////////
+function MySolution2 (n) {
+  if (n === 0) {
+    return 0
+  }
+
+  if (n === 1) {
+    return 1
+  }
+
+  return my_solution_2(n - 1) + my_solution_2(n - 2)
+}
+
+/////////////////
+// Memoization //
+/////////////////
+function MySolution3 (n) {
+  const fib_map = {}
+
+  const fib =  (n) => {
+    if (n < 2) {
+      return n
+    }
+
+    if (fib_map[n]) {
+      return fib_map[n]
+    } else {
+      fib_map[n] = fib(n - 1) + fib(n - 2)
+      return fib_map[n]
+    }  
+  }
+
+  this.execute = () => {
+    return fib(n)
+  }
+}
 
 function fib(n) {
   return new MySolution3(n).execute()

5a5b53332f97a8d3906b15ba325ef658103ce78c
Heunsig <heun3344@gmail.com>
2019-08-22 14:19:31 +0900

    Removed other solutions

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -12,12 +12,18 @@
 //   4
 //   buzz
 
-
-const MySolution = require('./MySolution')
-// const Solution = require('./Solution')
-
 function fizzBuzz(n) {
-  MySolution(n)
+  for (let i = 1 ; i <= n ; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz')
+    } else if (i % 5 === 0 ) {
+      console.log('buzz')
+    } else if (i % 3 === 0) {
+      console.log('fizz')
+    } else {
+      console.log(i)
+    }
+  }
 }
 
 module.exports = fizzBuzz;

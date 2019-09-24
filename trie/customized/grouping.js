1ac4bc08a6963df4c223d2232b11dcef37fdd1e9
Heunsig <heun3344@gmail.com>
2019-09-24 22:52:16 +0900

    need to make a function to block duplicated keys

--- trie/customized/grouping.js
+++ trie/customized/grouping.js
@@ -1,12 +1,13 @@
 class Node {
-  constructor (name) {
+  constructor (key, name) {
+    this.key = key
     this.name = name
     this.children = []
     this.is_last = false
   }
 
-  add (name) {
-    const node = new Node(name)
+  add (key, name) {
+    const node = new Node(key, name)
     this.children.push(node)
     return node
   }
@@ -14,7 +15,7 @@ class Node {
 
 class Trie {
   constructor () {
-    this.root = new Node(null)
+    this.root = new Node(null, null)
   }
 
   addKeys (keys) {
@@ -28,49 +29,40 @@ class Trie {
     const names = key.split('/')
 
     for (const [index, name] of names.entries()) {
-      let indexOfName = this.findNameNotLast(name, cursor.children)
+      let childKey = names.slice(0, index + 1).join('/')
 
-      if (indexOfName > -1) {
-        if (index === names.length - 1) {
-          const newNode = cursor.add(name)
-          cursor = newNode
-        } else {
-          cursor = cursor.children[indexOfName]  
-        }
+      if (index === names.length - 1) {
+        const newNode = cursor.add(childKey, name)
+        newNode.is_last = true
+        return newNode
+      }
+
+      let child = this.findChildOfName(name, cursor.children)
+      if (child) {
+        cursor = child
       } else {
-        const newNode = cursor.add(name)
+        const newNode = cursor.add(childKey, name)
         cursor = newNode
       }
     }
-
-    cursor.is_last = true
   }
 
-
-  findKey (key) {
+  findValueBy (key='') {
     let cursor = this.root
     const names = key.split('/')
 
     for (const [index, name] of names.entries()) {
       if (index === names.length - 1) {
-        let indexOfName = this.findNameLast(name, cursor.children)
-        if (indexOfName > -1) {
-          return cursor.children[indexOfName]  
-        }
+        return this.findChildOfName(name, cursor.children, true)
+      }
 
-        return null
-        
-      } 
-
-      let indexOfName = this.findNameNotLast(name, cursor.children)
-      if (indexOfName > -1) {
-        cursor = cursor.children[indexOfName]  
+      let child = this.findChildOfName(name, cursor.children)
+      if (child) {
+        cursor = child
       } else {
         return null
       }
     }
-
-    return null
   }
 
   list (key='') {
@@ -78,37 +70,26 @@ class Trie {
     const names = key.split('/')
 
     for (let name of names) {
-      let indexOfName = this.findNameNotLast(name, cursor.children)
-      if (indexOfName > -1) {
-        cursor = cursor.children[indexOfName]
+      let child = this.findChildOfName(name, cursor.children)
+      if (child) {
+        cursor = child
       } else {
         return null
       }
     }
 
-    return cursor
+    return cursor.children
   }
 
-  findNameNotLast (name, objs) {
-    for (let obj of objs) {
-      if (obj.name === name && obj.is_last !== true) {
-        return objs.indexOf(obj)
+  findChildOfName (name, children, is_last=false) {
+    for (let child of children) {
+      if (child.name === name && child.is_last === is_last) {
+        return child
       }
     }
 
-    return -1
+    return null
   }
-
-  findNameLast (name, objs) {
-    for (let obj of objs) {
-      if (obj.name === name && obj.is_last === true) {
-        return objs.indexOf(obj)
-      }
-    }
-
-    return -1
-  }
-
 }
 
 const a = new Trie()
@@ -119,19 +100,20 @@ debugger
 a.addKey('test/name')
 a.addKey('test/name/test2')
 
-// console.log(a.list('name'))
+// console.log(a.list('test/name'))
 debugger
 a.addKey('test/name/test2')
 a.addKey('test/age')
 a.addKey('test/address')
-a.addKey('test/address')
+// a.addKey('test/address')
 a.addKey('test/address1/basic')
 a.addKey('test/address1/basic/hi')
 a.addKey('test/address/basic')
 a.addKey('test/address/basic/hi')
 a.addKey('good/address/basic/hi')
 
+// console.log(a.list('test/name'))
 // console.log(a.findKeysBy('test/address1'))
-console.log(a.findKey('test/address1/basic'))
-// console.log(a.list('test'))
+// console.log(a.findValueBy(''))
+// console.log(a.list('test/address1'))
 // console.log(a.list('test/name'))
